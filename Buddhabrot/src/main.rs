3ecbd5b68b6ab59a4db6eebcfe54ad0f4a250ce8
A. Kim <kingalvinjkim@gmail.com>
2023-11-26 05:42:27 -0500

    Improved buddhabrot images

--- Buddhabrot/src/main.rs
+++ Buddhabrot/src/main.rs
@@ -2,14 +2,12 @@
 
 pub mod buddhabrot;
 pub mod complex;
+pub mod image_handler;
 pub mod mandelbrot;
 pub mod math;
 pub mod multithread_demo;
-pub mod save_raw_to_png;
 
 use std::time::Instant;
-
-use crate::mandelbrot::mandelbrot_grid::default_settings;
 
 fn main() {
     let start = Instant::now();
@@ -32,22 +30,37 @@ fn main() {
     //     &String::from("outputs/buddhabrot/test/multithread_1/multithread_1.png"),
     // );
 
-    save_raw_to_png::save_rgb_raw_to_png(
-        buddhabrot::rgb_buddhabrot::triple_density_map_to_rgb(
-            &buddhabrot::rgb_buddhabrot::get_buddhabrot_triple_density_map_random_samples_multithreaded(
-                4092,
-                default_settings::X_SAMPLE_RANGE,
-                4092,
-                default_settings::Y_SAMPLE_RANGE,
-                1000000000,
-                2,
-                &[5000, 500, 50],
-                5
-            ),
-        ),
+    // image_handler::save_rgb_raw_to_png(
+    //     buddhabrot::rgb_buddhabrot::triple_density_map_to_rgb(
+    //         &buddhabrot::rgb_buddhabrot::get_buddhabrot_triple_density_map_random_samples_multithreaded(
+    //             4092,
+    //             default_settings::X_SAMPLE_RANGE,
+    //             4092,
+    //             default_settings::Y_SAMPLE_RANGE,
+    //             1000000000,
+    //             2,
+    //             &[5000, 500, 50],
+    //             5
+    //         ),
+    //     ),
+    //     4092,
+    //     4092,
+    //     &String::from("outputs/buddhabrot/test/multithread_3/multithread_3.png"),
+    // );
+
+    let data =
+        image_handler::get_png_as_raw_rgb(&String::from("outputs/buddhabrot/curved/curved.png"));
+
+    image_handler::save_rgb_raw_to_png(
+        (0..data.len())
+            .into_iter()
+            .map(|index| {
+                data[4092 * 3 * ((index / 3) % 4092) + 3 * ((index / 3) / 4092) + index % 3]
+            })
+            .collect(),
         4092,
         4092,
-        &String::from("outputs/buddhabrot/test/multithread_3/multithread_3.png"),
+        &String::from("outputs/buddhabrot/transposed/transposed.png"),
     );
 
     println!("Elapsed: {:.2?}", start.elapsed());
--- README.md
+++ README.md
@@ -8,15 +8,14 @@ This is the rust-generated one:
 ![rust-generated-dist-sqrd-method-500-iter-4096x4096](Buddhabrot/outputs/mandelbrot/colors/test.png)
 
 This is the best Buddhabrot.
-![rust-generated-buddhabrot](Buddhabrot/outputs/buddhabrot/test/multithread_3/multithread_3.png)
+![rust-generated-buddhabrot](Buddhabrot/outputs/buddhabrot/transposed/transposed.png)
 
-TODO: Include extra information in outputs (such as logging the time taken and the parameters)
 TODO: save the raw data to make it easy to add more data iteratively. (is there an algorithm to find a new point in an area that hasn't been covered much so far, the easy way would just be to create a completely random point and after a while it will balance out but IDK)
 TODO: use the fact that buddhabrot and mandelbrot are symmetrical
-TODO: Optimize mandelbrot, maybe use a profiler and multithread
 TODO: change the Option type thing for default settings, just make it a macro
 TODO: Julia sets (combine Julia and mandelbrot)
 TODO: implement more fractals from [3B1B](https://www.youtube.com/watch?v=LqbZpur38nw&t=31s)
 TODO: Chaos game
 TODO: L-system
 TODO: Logistic map
+TODO: Anti-mandelbrot and other similar things (where the c is f(point) and f can be any function. in the normal mandelbrot, f(c) = c)
